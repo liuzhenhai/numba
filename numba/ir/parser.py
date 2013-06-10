3c09a51a3a802fda0002c8784c72c0bc2dd39129
Mark Florisson <markflorisson88@gmail.com>
2013-06-10 16:41:27 +0100

    Enhance and test IR parser

--- numba/ir/parser.py
+++ numba/ir/parser.py
@@ -6,27 +6,19 @@ Parse numba IR.
 
 from __future__ import print_function, division, absolute_import
 
+from collections import namedtuple
 from lepl import *
+
+# ______________________________________________________________________
+
+Function = namedtuple("Function", ["name", "args", "blocks"])
+Block = namedtuple("Block", ["label", "stats"])
+Stat = namedtuple("Stat", ["dest", "opname", "type", "args"])
+
+# ______________________________________________________________________
 
 def lit(name):
     return Drop(Literal(name))
-
-class ParserActions(object):
-    def __init__(self):
-        self.stats = []
-        self.argtypes = []
-        self.argnames = []
-
-    def p_header(self, args):
-        name, arglist = args[0], args[1:]
-        self.funcname = name
-        for typename, argname in arglist:
-            self.argtypes.append(typename)
-            self.argnames.append(argname)
-
-    def p_stat(self, args):
-        dest, opname, type, arglist = args
-        self.stats.append((dest, opname, type, arglist))
 
 par = lambda value: lit("(") & value & lit(")")
 
@@ -35,22 +27,64 @@ nl = ~Newline()
 ident = Regexp("[a-zA-Z0-9_]+")
 value = lit("%") & ident
 
-def parse(actions):
+# ______________________________________________________________________
+
+def p_header(args):
+    name, arglist = args[0], args[1:]
+    args = list(map(tuple, arglist)) # [(argname, argtype)]
+    return Function(name, args, [])
+
+def p_block(args):
+    label, = args
+    return Block(label, [])
+
+def p_stat(args):
+    dest, type, opname, arglist = args
+    return Stat(dest, opname, type, list(map(tuple, arglist)))
+
+# ______________________________________________________________________
+
+repeat = lambda x: Star(x|ws & nl) & ws
+
+def parse(source):
+    "Return a list of parsed statements (Function|Block|Stat)"
     # TODO: Parse grammar as a string and then use lepl
     with Separator(ws):
         type = ident
         arg = type & value >List
         args = Optional(arg) & Star(lit(",") & arg)
         arglist = par(args)
-        header = lit("function") & ident & arglist >actions.p_header
+        header = lit("function") & ident & arglist >p_header
         statargs = arglist >List
-        stat = value & lit("=") & par(type) & ident & statargs >actions.p_stat
-        stats = Star((stat|ws) & nl)
-        function = header & lit("{") & nl & stats & lit("}") & ws
+        stat = ws & value & lit("=") & par(type) & ident & statargs & nl >p_stat
+        block = ws & ident & lit(":") >p_block
+        stats = repeat(block|stat)
+        function = header & lit("{") & nl & block & nl & stats & lit("}") & ws
+        functions = repeat(function)
 
-    print(function.parse("""function func(int %foo) {
-        %1 = (double) foo(int %foo)
-    }
-    """))
+    # stats.parse(source)
+    return functions.parse(source)
 
-parse(ParserActions())
+# ______________________________________________________________________
+
+def build(parse_result):
+    functions = []
+    function, stats = parse_result[0], parse_result[1:]
+
+    # Populate Functions and Blocks
+    for stat in parse_result:
+        if isinstance(stat, Function):
+            function = stat
+            functions.append(function)
+        elif isinstance(stat, Block):
+            block = stat
+            function.blocks.append(block)
+        else:
+            block.stats.append(stat)
+
+    return functions
+
+# ______________________________________________________________________
+
+def from_assembly(source):
+    return build(parse(source))
---
+++ numba/ir/tests/test_parser.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import
+
+from numba.ir import parser
+
+source = """
+
+function func(int %foo) {
+entry:
+    %1 = (double) foo(int %foo)
+ block:
+    %2 = (double) scotch(int %foo)
+
+}
+
+function bar(int %blah) {
+entry:
+    %1 = (double) ham(int %foo)
+block:
+    %2 = (double) eggs(int %foo)
+}
+
+"""
+
+def test_parse():
+    result = parser.from_assembly(source)
+    func1, func2 = result
+
+    assert func1.name == 'func'
+    assert func1.args == [("int", "foo")]
+    assert len(func1.blocks) == 2
+    stat = parser.Stat("1", "foo", "double", [("int", "foo")])
+    assert func1.blocks[0].stats == [stat], func1.blocks[0].stats
+
+    assert func2.name == 'bar'
+    assert func2.args == [("int", "blah")]
+    assert len(func2.blocks) == 2
+    stat = parser.Stat("1", "ham", "double", [("int", "foo")])
+    assert func2.blocks[0].stats == [stat], func1.blocks[0].stats
+
+
+test_parse()
